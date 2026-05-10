a5d6619e4944914c4b47f122d6edd6e4b7c22950
AGX <agxnh2020@gmail.com>
2026-05-10 19:09:46 +0300

    added the request header parsing to tcp listener

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"log/slog"
 	"net"
 	"os"
-	"strings"
+
+	"github.com/AGX18/httpServer-1.1/internal/request"
 )
 
 func main() {
@@ -28,45 +27,16 @@ func main() {
 			logger.Error(err.Error(), "remote_addr", conn.RemoteAddr())
 		}
 		fmt.Println("a connection has been accepted")
-		lines := getLinesChannel(conn)
-		for line := range lines {
-			fmt.Println(line)
+		req, err := request.RequestFromReader(conn)
+		if err != nil {
+			logger.Error(err.Error())
 		}
+		fmt.Printf("Request line: \n- Method: %s\n- Target: %s\n- Version: %s\n",
+			req.RequestLine.Method,
+			req.RequestLine.RequestTarget,
+			req.RequestLine.HttpVersion,
+		)
 		conn.Close()
 		fmt.Println("the connection is closed")
 	}
-}
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	strCh := make(chan string, 8)
-
-	currentLineContents := ""
-	buffer := make([]byte, 8)
-	go func() {
-		for {
-			n, err := f.Read(buffer)
-			if err != nil {
-				if errors.Is(err, io.EOF) {
-					currentLineContents += string(buffer[:n])
-					if currentLineContents != "" {
-						strCh <- fmt.Sprintf("%s", currentLineContents)
-						currentLineContents = ""
-					}
-					break
-				}
-				fmt.Printf("error: %s\n", err.Error())
-				break
-			}
-			str := string(buffer[:n])
-			parts := strings.Split(str, "\n")
-			for i := 0; i < len(parts)-1; i++ {
-				strCh <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
-				currentLineContents = ""
-			}
-			currentLineContents += parts[len(parts)-1]
-		}
-		close(strCh)
-	}()
-
-	return strCh
 }
